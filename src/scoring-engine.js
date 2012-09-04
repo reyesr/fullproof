24b4306688fa1d95e33f8b359695dda84d2cf4e0
reyesr <reyes.rr@gmail.com>
2012-09-04 18:50:08 +0200

    scoring engine

---
+++ src/scoring-engine.js
@@ -0,0 +1,120 @@
+/*
+ * Copyright 2012 Rodrigo Reyes
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+var fullproof = fullproof||{};
+
+/**
+ * This engine is based on scoring. During the injection of document, the parser must provide {fullproof.ScoredElement} 
+ * instances instead of primary values. The score is used to sort the results.
+ * 
+ * @constructor
+ */
+fullproof.ScoringEngine = function() {
+	if (!(this instanceof fullproof.ScoringEngine)) {
+		return new fullproof.ScoringEngine();
+	}
+	
+	this.initAbstractEngine();
+}
+
+fullproof.AbstractEngine = fullproof.AbstractEngine || (function() {});
+fullproof.ScoringEngine.prototype = new fullproof.AbstractEngine;
+
+fullproof.ScoringEngine.prototype.checkCapabilities = function(capabilities, analyzer) {
+	if (capabilities.getUseScores() !== true || analyzer.provideScore !== true) {
+		return false;
+	}
+	if (!capabilities.getComparatorObject()) {
+		return false;
+	}
+	return true;
+}
+
+fullproof.ScoringEngine.prototype.lookup = function(text, callback) {
+	
+//	function mergeScoreFn(score1,score2) {
+//		return score1 + score2;
+//	}
+//	
+//	function merge_by_score(rset1, rset2) {
+//		var i1=0, max1=rset1.getSize(), 
+//			i2=0, max2=rset2.getSize(),
+//			obj1=null,obj2=null;
+//		var comp = rset1.comparatorObject;
+//		var result = [];
+//		while (i1 < max1 && i2<max2) {
+//			obj1 = rset.getItem(i1);
+//			obj2 = rset.getItem(i2);
+//			if (comp.equals(obj1, obj2)) {
+//				// Merge scores
+//				result.push(new fullproof.ScoredElement(obj1.value, mergeScoreFn(obj1.score, obj2.score)));
+//			} else if (comp.lower_than(obj1,obj2)) {
+//				result.push(obj1);
+//				++i1;
+//			} else {
+//				result.push(obj2);
+//				++i2;
+//			}
+//		}
+//		while (i1 < max1) {
+//			result.push(obj1);
+//			++i1;
+//		}
+//		while (i2 < max2) {
+//			result.push(obj2);
+//			++i2;
+//		}
+//		return result;
+//	};
+	
+	function merge_resultsets(rset_array, storeDesc) {
+		if (rset_array.length == 0) {
+			return new fullproof.ResultSet(storeDesc.caps.getComparatorObject());
+		} else {
+			var set = rset_array.shift();
+			while (rset_array.length > 0) {
+				set.merge(rset_array.shift(), fullproof.ScoredElement.mergeFn);
+			}
+			return set;
+		}
+	}
+	
+	var synchro_all_indexes = fullproof.make_synchro_point(function(array_of_resultset) {
+		var merged = merge_resultsets(array_of_resultset);
+		callback(merged);
+	}, this.stores.length);
+	
+	for (var i=0; i<this.stores.length; ++i) {
+		var store = this.stores[i];
+		store.parser.parse(text, fullproof.make_synchro_point(function(array_of_words) {
+			if (array_of_words) {
+					if (array_of_words.length == 0) {
+						callback(new fullproof.ResultSet(store.caps.getComparatorObject()));
+					} else {
+						var lookup_synchro = fullproof.make_synchro_point(function(rset_array) {
+							var merged = merge_resultsets(rset_array, store.caps.getComparatorObject());
+							synchro_all_indexes(merged);
+						}, array_of_words.length);
+		
+						for (var i=0; i<array_of_words.length; ++i) {
+							store.index.lookup(array_of_words[i].key, lookup_synchro);
+						}
+					}
+				}
+		}));
+
+	}
+};
---
+++ tests/test-scoring-engine.js
@@ -0,0 +1,73 @@
+
+var corpus = {
+		1: "first line of data",
+		2: "second line",
+		3: "third line of data",
+		4: "four in a row of data",
+		5: "high five"
+};
+
+function initializerFunc(injector, callback) {
+	var synchro = fullproof.make_synchro_point(callback, 5);
+	for (var k in corpus) {
+		injector.inject(corpus[k], k, synchro);
+	}
+} 
+
+test("create scoring engine", function() {
+	var engine = new fullproof.ScoringEngine();
+	expect(1);
+	QUnit.stop();
+	engine.clear(function() {
+		ok(!!engine);
+		QUnit.start();
+	});
+});
+
+test("create scoring engine", function() {
+	var engine = new fullproof.ScoringEngine();
+	expect(1);
+	QUnit.stop();
+	engine.clear(function() {
+		ok(!!engine);
+		QUnit.start();
+	});
+});
+
+test("clear index", function() {
+	var engine = new fullproof.ScoringEngine();
+	expect(1);
+	QUnit.stop();
+	engine.addIndex("myindex", 
+			new fullproof.ScoringAnalyzer, 
+			new fullproof.Capabilities().setStoreObjects(false).setUseScores(true).setComparatorObject(fullproof.ScoredElement.comparatorObject), 
+			initializerFunc, 
+			function(index) {
+		engine.clear(function() {
+			engine.lookup("third", function(resultset) {
+				equal(resultset, false);
+				QUnit.start();
+			});
+		});
+	});
+});
+
+test("create one index", function() {
+	var engine = new fullproof.ScoringEngine();
+	QUnit.stop();
+	engine.addIndex("myindex", 
+			new fullproof.ScoringAnalyzer, 
+			new fullproof.Capabilities().setStoreObjects(false).setUseScores(true).setComparatorObject(fullproof.ScoredElement.comparatorObject), 
+			initializerFunc, 
+			function(index) {
+		ok(!!index);
+		
+		engine.lookup("third", function(resultset) {
+			ok(!!resultset);
+			equal(resultset.getSize(), 1);
+			equal(parseInt(resultset.getItem(0).value), 3);
+			ok(!!resultset);
+			QUnit.start();
+		});
+	});
+});
